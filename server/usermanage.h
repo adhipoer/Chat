51e37b2af3c3d406a6e97a4119aa6417042f12b1
samihd <debepe.10@gmail.com>
2015-03-09 01:11:57 +0700

    edit usermanage.h

--- server/usermanage.h
+++ server/usermanage.h
@@ -1,10 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <pthread.h>  
 
-typedef struct user{
-	char name[20];
-	char pass[20];
-	struct user *next;
-}user_n;
+typedef struct user user;
+typedef struct list list;
 
+struct user{
+	user *next;
+	char *username;
+	char *userpass;
+};
+struct list{
+	user *head;
+};
 
+void init(list *);
+void adduser(list *, char *, char *, pthread_mutex_t *);
+void* removeItem(list *,  char *, char *, pthread_mutex_t *);
+void* removeThread(list *list, pthread_t data, pthread_mutex_t *mutex);
+int find(list *, char *, char *, pthread_mutex_t *);
