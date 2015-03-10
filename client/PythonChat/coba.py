9efbaa2a4171052ad43379b7b6fbfff324a18933
frhdina <izdiharfarahdina@gmail.com>
2015-03-10 20:50:44 +0700

    lagi stuck.
    udah message, without GUI and usermanagement.

--- client/PythonChat/coba.py
+++ client/PythonChat/coba.py
@@ -17,4 +17,25 @@ def client():
     execpt:
         print'sorry, unable to connect'
         sys.exit()
-    
+    print 'you can start sending message'
+        duringyou()
+
+    while 1:
+        socket_list = [sys.stdin, s]
+        read,write,in_error = select.select(socket_list , [], [])
+        for sock in read:
+            if sock == s:
+                data = sock.recv(4096)
+                    if not data :
+                        print '\nDisconnected'
+                        sys.exit()
+                    else :
+                        sys.stdout.write(data)
+                        duringyou()
+            else:
+                message = sys.stdin.readline()
+                s.send(message)
+                duringyou()
+                  
+if __name__ == "__main__":
+    sys.exit(client())
