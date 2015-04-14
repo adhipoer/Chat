5e990eead5a1e5cba2903bd8bf40f25f24ca8cb5
frhdina <izdiharfarahdina@gmail.com>
2015-04-14 21:09:17 +0700

    Revert "deletes"
    This reverts commit 4e76fe24f7bb2c78b3c7dbc490cca4bf3f47919d.

---
+++ AES/RectangularArrays.cs
@@ -0,0 +1,23 @@
+﻿
+internal static partial class RectangularArrays
+{
+    internal static sbyte[][] ReturnRectangularSbyteArray(int size1, int size2)
+    {
+        sbyte[][] newArray;
+        if (size1 > -1)
+        {
+            newArray = new sbyte[size1][];
+            if (size2 > -1)
+            {
+                for (int array1 = 0; array1 < size1; array1++)
+                {
+                    newArray[array1] = new sbyte[size2];
+                }
+            }
+        }
+        else
+            newArray = null;
+
+        return newArray;
+    }
+}
